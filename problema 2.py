f52b6a59070625769ce4c352888e26211229ba98
Edson-dev146 <felix.e@escolar.ifrn.edu.br>
2026-05-22 15:57:39 -0300

    RESOLVENDO O PROBLEMA 2 ( AGORA DEU CERTO )

---
+++ problema 2.py
@@ -0,0 +1,11 @@
+print("Um vestido que custa R$ 540,00 tem desconto de 20% se comprado à vista. Qual é o preço à vista do vestido?")
+
+
+vestido = 540
+
+desconto = 2/10
+
+resultado = vestido * desconto 
+
+print(resultad, " é o valor do desconto do vestido")
+
